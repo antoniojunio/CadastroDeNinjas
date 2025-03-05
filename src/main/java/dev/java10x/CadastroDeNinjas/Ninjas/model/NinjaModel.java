5c64d85e218c8d9e3a860c102aca2d6f133f66a7
antoniojunio <juniordsj@gmail.com>
2025-03-05 14:41:38 -0300

    feat: Coloca o Lombok na classe para abstracao

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/model/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/model/NinjaModel.java
@@ -2,9 +2,15 @@ package dev.java10x.CadastroDeNinjas.Ninjas.model;
 
 import dev.java10x.CadastroDeNinjas.Missoes.model.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
     @Id
@@ -17,40 +23,5 @@ public class NinjaModel {
     @ManyToOne
     @JoinColumn(name = "missoes_id")
     private MissoesModel missoes;
-
-    public NinjaModel() {
-
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
 
 }
